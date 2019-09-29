9aa7b81da1d08ee4218566f6d9fc771c230e3215
BadrAbdi <babdi@my.yorku.ca>
2019-09-28 21:46:55 -0400

    update

--- cex1d.asm
+++ cex1d.asm
@@ -1,24 +1,35 @@
 ;find all divisors
 s0: DC "Find all divisors."
 s1: DC "Enter i:\0"
-s3: DC "Divisors:\0"
+s2: DC "Divisors:\0"
+s3: DC "prime"
+s4: DC "not prime"
 addi x28, x0, 0
 addi x5, x0, s0
 ecall x0, x5, 4 ;prompt s0
 addi x5, x0, s1
-ecall x0, x5, 4
+ecall x1, x5, 4
 ecall x6, x0, 5 ;input for n
 addi x8, x0, 0
-ld x5, s3(x0)
-addi x5, x0, s3
+addi x5, x0, s2
 ecall x0, x5, 4 ;prompt s0
 loop: addi x8, x8, 1
       rem x11, x6, x8
       bne x11, x0, loop
       add x29, x28, x0
       sd x8, 0(x29)
-      ecall x0, x8,0
+      ecall x0, x29,0
       addi x28, x28, 8 
       bne x8, x6, loop
+
+ld x7, 8(x0)
+beq x7, x6, src
+addi x5, x0, s4
+ecall x0, x5, 4
+
+src:
+addi x5,x0, s3 
+ecall x0, x5, 4
+     
 
 
