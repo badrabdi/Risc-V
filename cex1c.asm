7fd7fe2c0e4d07b3c472ee909f0fec7b3f7c113e
BadrAbdi <babdi@my.yorku.ca>
2019-09-28 20:59:15 -0400

    Update cex1c.asm

--- cex1c.asm
+++ cex1c.asm
@@ -6,12 +6,13 @@ addi x28, x0, 0
 addi x5, x0, s0
 ecall x1, x5, 4 ;prompt s0
 ecall x6, x0, 5 ;input for n
-add x8, x8, x6
-loop: rem x11, x6, x8
-      beq x11, x0, src
-      addi x8, x8, -1
-      bne x8, x0, loop
-src:  add x29, x28, x0
-      sd 0(x29), x8
+addi x8, x0, 0
+loop: addi x8, x8, 1
+      rem x11, x6, x8
+      bne x11, x0, loop
+      add x29, x28, x0
+      sd x8, 0(x29)
       addi x28, x28, 8 
+      bne x8, x6, loop
+
 
